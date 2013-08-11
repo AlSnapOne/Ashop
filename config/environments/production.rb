08281f8416508dc5b8ac6f25541c9f35bf32e7bc
Georgios Kampouris <alsnapone@hotmail.gr>
2013-08-11 20:26:02 +0100

    problem

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -15,7 +15,7 @@ Ashop::Application.configure do
   config.assets.compress = true
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
-  config.assets.compile = true
+  config.assets.compile = false
 
   # Generate digests for assets URLs
   config.assets.digest = true
@@ -47,7 +47,7 @@ Ashop::Application.configure do
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
    config.assets.precompile += %w( two-column.css )
-
+   config.assets.precompile += %w( application.css )
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
 
