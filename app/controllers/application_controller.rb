f54e153baee0f93e854d88244fdd084a842212d2
Georgios Kampouris <alsnapone@hotmail.gr>
2013-08-11 18:48:46 +0100

    yo int

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,9 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
-
+before_filter :set_locale
+def set_locale
+  I18n.local = params[:lang] if params[:lang].present?
+end
 	def after_sign_in_path_for(resource)
 	"/myprofile"
 	end
